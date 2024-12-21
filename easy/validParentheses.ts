c345c3a4362d10737b26d2c2d0a2466cea30b222
Vignesh Iyer <vgnshiyer@gmail.com>
2024-12-21 10:50:40 -0800

    [easy] valid palindrome

---
+++ easy/validParentheses.ts
@@ -0,0 +1,23 @@
+function isValid(s: string): boolean {
+    const stack: string[] = [];
+    for (let c of s) {
+        if (c === "(" || c === "[" || c === "{") stack.push(c);
+        else {
+            if (c == ")") {
+                if (stack.length == 0 || stack[stack.length - 1] != "(") return false;
+            }
+
+            if (c == "]") {
+                if (stack.length == 0 || stack[stack.length - 1] != "[") return false;
+            }
+
+            if (c == "}") {
+                if (stack.length == 0 || stack[stack.length - 1] != "{") return false;
+            }
+
+            stack.pop();
+        }
+    }
+    return stack.length == 0;
+};
+
